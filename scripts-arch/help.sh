ede640fe23d5e65e9a61cc32d96961adc5d5c080
nwg-piotr <nwg.piotr@gmail.com>
2018-12-19 15:07:20 +0100

    weather help added

--- scripts-arch/help.sh
+++ scripts-arch/help.sh
@@ -14,6 +14,7 @@ echo "--lbrightness [up] | [down] | [<level>] [-N] (for 'light-git' optional pac
 echo "--battery [-l] | [-N]"
 echo "--wifi [-N] | [-M'custom name']"
 echo "--update -C[aur_helper] | -U<terminal>[:aur_helper] | [-O] [-N] | [-M<custom_name] | [-h] | [--help]"
+echo "--weather [-I<items>] [-A<api_key>] [-C<city_id>] [-U<units>] [-L<lang>] | [-D[<city_id>]] | [-h] | [--help]"
 printf "\n[menu] argument after --command displays context menu (if jgmenu package installed). Edit templates in ~/.t2ecol folder to customize menus.\n"
 printf "\n[-N] - displays name instead of icon in tint2 panel. Uncheck 'Show icon' in executor!\n\n"
 echo "In --battery command [-l] - appends level text to the icon | [-N] displays textual name and level instead of icon"
